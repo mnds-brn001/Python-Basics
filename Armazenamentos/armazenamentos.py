a3dce5324e128b2d27ee2a8eef4b5b0c4e48974d
BrunoM <112877836+mnds-brn001@users.noreply.github.com>
2024-03-31 23:44:25 -0300

    Adicionado arquivo python
    Armazenamento de variável em Python

---
+++ Armazenamentos/armazenamentos.py
@@ -0,0 +1,35 @@
+# Declaração   das variáveis do programa
+# A, B, C, D,  são declaradas na atribuilção de valores
+
+#atribui o valor de 0 à variável D
+
+D = 0
+# exibe na tela o valor de D com frase explicativaa
+print("Valor de D: ", D)
+
+#atribui o valor de 2 à variavel A
+A = 2
+# exibe na tela o valor de A com frase explicativa.
+print("Valor de A: ", A)
+
+#atribui o valor 3 à variável B
+B = 3
+#exibe na tela o valor de B com frase explicativa
+print("Valor de B: ", B)
+
+#atribui o valor de 5 à variável C
+C = 5
+# exibe na tela o valor de C com frase explicativa]
+print("Valor de C: ", C)
+
+#atualiza a variável C  o seu valor anterior acrescido de 1
+C = C + 1
+
+# exibe na tela o valor de C (atualizado) com frase explicativa
+print("Valor de C: ", C)
+
+# atribui à variavel D a soma do valor de A com o valor atual de C
+D = A + C
+
+# exibe na tela o valor de D (atualizado) com frase explicativa.
+print("Valor de D: ", D)
